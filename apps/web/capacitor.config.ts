0fb6c5f90d35b79c71c0f16bbcf2c5df518ed966
Nghi Dang <dangquocnghi@gmail.com>
2026-08-23 19:17:18 +0700

    Initialize your Capacitor config

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.micra.app',
+  appName: 'micra',
+  webDir: 'build'
+};
+
+export default config;
